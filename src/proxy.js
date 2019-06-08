7eb74d008b00b47619227bb149ca184eb5c33a8b
Mykhailo Maslov <mykhailom@wix.com>
2019-06-08 11:36:08 +0300

    Create bot

--- src/proxy.js
+++ src/proxy.js
@@ -1,25 +1,29 @@
 "use strict";
 
 const proxify = (provider, request, response) => {
-  const proxyOptions = {
-    headers: request.headers,
-    method: request.method
-  };
-  const proxy = provider.request(request.url, proxyOptions);
 
-  request.pipe(
-    proxy,
-    { end: true }
-  );
+    const proxyOptions = {
+            "headers": request.headers,
+            "method": request.method
+        },
+        proxy = provider.request(request.url, proxyOptions);
 
-  proxy.on("response", res => {
-    res.headers["my-data"] = "some_val";
-    response.writeHead(res.statusCode, res.statusMessage, res.headers);
-    res.pipe(
-      response,
-      { end: true }
+    request.pipe(
+        proxy,
+        {"end": true}
     );
-  });
+
+    proxy.on("response", (res) => {
+
+        res.headers["my-data"] = "some_val";
+        response.writeHead(res.statusCode, res.statusMessage, res.headers);
+        res.pipe(
+            response,
+            {"end": true}
+        );
+
+    });
+
 };
 
 module.exports = proxify;
